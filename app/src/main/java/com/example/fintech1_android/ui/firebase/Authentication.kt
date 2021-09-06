cf0da7a92c8525d4bad87e889c42a0635639e6b4
ENRAG3DCHICKEN <yiu.jackie@gmail.com>
2021-09-06 00:15:17 -0400

    Clean Build - Connected Firebase Signup/Login to Charge Payment Screen

--- app/src/main/java/com/example/fintech1_android/ui/firebase/Authentication.kt
+++ app/src/main/java/com/example/fintech1_android/ui/firebase/Authentication.kt
@@ -6,11 +6,12 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavHostController
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
-fun SignupUser(context: ComponentActivity, email: String, password: String) {
+fun SignupUser(navController: NavHostController, context: ComponentActivity, email: String, password: String) {
 
     lateinit var auth: FirebaseAuth
 
@@ -23,6 +24,9 @@ fun SignupUser(context: ComponentActivity, email: String, password: String) {
                 // Sign in success, update UI with the signed-in user's information
                 Log.d(ContentValues.TAG, "createUserWithEmail:success")
                 val user = auth.currentUser
+
+                navController.navigate("ChargePayment")
+
             } else {
                 // If sign in fails, display a message to the user.
                 Log.w(ContentValues.TAG, "createUserWithEmail:failure", task.exception)
@@ -32,7 +36,7 @@ fun SignupUser(context: ComponentActivity, email: String, password: String) {
         }
 }
 
-fun LoginUser(context: ComponentActivity, email: String, password: String) {
+fun LoginUser(navController: NavHostController, context: ComponentActivity, email: String, password: String) {
 
     lateinit var auth: FirebaseAuth
 
@@ -45,6 +49,9 @@ fun LoginUser(context: ComponentActivity, email: String, password: String) {
                 // Sign in success, update UI with the signed-in user's information
                 Log.d(TAG, "signInWithEmail:success")
                 val user = auth.currentUser
+
+                navController.navigate("ChargePayment")
+
             } else {
                 // If sign in fails, display a message to the user.
                 Log.w(TAG, "signInWithEmail:failure", task.exception)
--- app/src/main/java/com/example/fintech1_android/ui/views/Landing.kt
+++ app/src/main/java/com/example/fintech1_android/ui/views/Landing.kt
@@ -2,41 +2,59 @@ package com.example.fintech1_android.ui.views
 
 import androidx.activity.ComponentActivity
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.fintech1_android.ui.firebase.SignupUser
 import com.example.fintech1_android.ui.firebase.LoginUser
 
 @Composable
 fun Landing(navController: NavHostController) {
-    Column {
-        Text("Flare Pay Merchants")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(
+            text = "Flare Pay Merchants",
+            modifier = Modifier.padding(25.dp)
+        )
 
         Button(
+            modifier = Modifier.padding(25.dp),
             onClick = { navController.navigate("Signup") }
         ) {
             Text(text = "Signup")
         }
 
         Button(
+            modifier = Modifier.padding(25.dp),
             onClick = { navController.navigate("Login") }
         ) {
             Text(text = "Login")
         }
 
         //TEST
-        Button(
-            onClick = { navController.navigate("ChargePayment") }
-        ) {
-            Text(text = "Charge Payment")
-        }
-
+//        Button(
+//            onClick = { navController.navigate("ChargePayment") }
+//        ) {
+//            Text(text = "Charge Payment")
+//        }
     }
+
 }
+
 
 @Composable
 fun Signup(navController: NavHostController, context: ComponentActivity) {
@@ -44,19 +62,26 @@ fun Signup(navController: NavHostController, context: ComponentActivity) {
     var text_email by remember { mutableStateOf("") }
     var text_password by remember { mutableStateOf("") }
 
-    Column {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         OutlinedTextField(
+            modifier = Modifier.padding(25.dp),
             value = text_email,
             onValueChange = { text_email = it },
             label = { Text("Email") }
         )
         OutlinedTextField(
+            modifier = Modifier.padding(25.dp),
             value = text_password,
             onValueChange = { text_password = it },
             label = { Text("Password") }
         )
         Button(
-            onClick = { SignupUser(context, text_email, text_password) }
+            modifier = Modifier.padding(25.dp),
+            onClick = { SignupUser(navController, context, text_email, text_password) }
         ) {
             Text("Submit")
         }
@@ -69,19 +94,26 @@ fun Login(navController: NavHostController, context: ComponentActivity) {
     var text_email by remember { mutableStateOf("") }
     var text_password by remember { mutableStateOf("") }
 
-    Column {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         OutlinedTextField(
+            modifier = Modifier.padding(25.dp),
             value = text_email,
             onValueChange = { text_email = it },
             label = { Text("Email") }
         )
         OutlinedTextField(
+            modifier = Modifier.padding(25.dp),
             value = text_password,
             onValueChange = { text_password = it },
             label = { Text("Password") }
         )
         Button(
-            onClick = { LoginUser(context, text_email, text_password) }
+            modifier = Modifier.padding(25.dp),
+            onClick = { LoginUser(navController, context, text_email, text_password) }
         ) {
             Text("Submit")
         }
